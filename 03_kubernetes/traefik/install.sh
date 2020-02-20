1e990c73236c83823290b9d1420036fb9e03c639
ismaelperal <ismaelperal@gmail.com>
2020-02-20 20:32:59 +0000

    add traefik cheese example for ingress

--- 03_kubernetes/traefik/install.sh
+++ 03_kubernetes/traefik/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 kubectl apply -f https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/traefik-rbac.yaml
-curl -LO "https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/traefik-deployment.yaml"
-curl -LO "https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/traefik-ds.yaml"
+
+# Download yamls
+# curl -LO "https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/traefik-ds.yaml"
+# curl -LO https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/ui.yaml
+# curl -LO https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/cheese-deployments.yaml
+# curl -LO https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/cheese-services.yaml
+# curl -LO https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/cheese-ingress.yaml
+# 
+
+kubectl apply -f ./
